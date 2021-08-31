4313ed014d391a46f43b1f8773a4822958dd5141
cagb2001 <cagb2001@gmail.com>
2021-08-30 21:56:11 -0500

    test03

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,8 +1,4 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-<<<<<<< HEAD
 print "A change is coming! ... tuvi!\n";
-=======
-print "A change is coming!\n";
->>>>>>> 1085a54cd3b29d3e874674bbd6b882f29efbd529
 ?>
